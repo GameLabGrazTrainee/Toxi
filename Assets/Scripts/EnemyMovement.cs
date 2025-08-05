69dc09e75efd3cf59dd775aedfbd278254c251ad
GameLabGrazTrainee <gamelabgraz.trainee@gmail.com>
2025-08-05 10:47:53 +0200

    Updated game version

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,20 +8,95 @@ public class EnemyMovement : MonoBehaviour
 {
     public Transform player;
     private NavMeshAgent navMeshAgent;
+    public bool isFollowingPlayer = true;
+    public List<Transform> targets = new List<Transform>();
+    private Transform currentTarget;
+    private float runawayTimer;
+    private float runawayInterval = 15f;
+    private Animator animator;
+    public AudioClip startClip;
+    public AudioClip stopClip;
+    public AudioClip ribbit;
+    public AudioClip coin;
+    public AudioSource audioSource;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        
         navMeshAgent = GetComponent<NavMeshAgent>();
-
+        animator = GetComponentInChildren<Animator>();
+        animator.SetBool("IsRunning", true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (player != null)
+        if (player != null && isFollowingPlayer == true)
         {
             navMeshAgent.SetDestination(player.position);
         }
+        else 
+        {
+            navMeshAgent.SetDestination(currentTarget.position);
+        }
+        if (isFollowingPlayer == false)
+        {
+            runawayTimer = runawayTimer + Time.deltaTime;
+            if (runawayTimer > runawayInterval)
+            {
+                runawayTimer = 0f;
+                startChasingPlayer();
+            }
+        }
+    }
+
+    public void chooseTarget()
+    {
+        int newTargetIndex = Random.Range(0, targets.Count);
+        Transform newTarget = targets[newTargetIndex];
+        if (newTarget == currentTarget)
+        {
+            chooseTarget();
+        }
+        else
+        {
+            currentTarget = newTarget;
+        }
+    }
+
+    public void stopChasingPlayer()
+    {
+        chooseTarget();
+        isFollowingPlayer = false;
+        navMeshAgent.speed = 8f;
+        audioSource.clip = stopClip;
+        audioSource.Play();
+    }
+
+    public void startChasingPlayer()
+    {
+        isFollowingPlayer = true;
+        navMeshAgent.speed = 3f;
+        audioSource.clip = startClip;
+        audioSource.Play();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("ToxiTarget") && isFollowingPlayer == false)
+        {
+            chooseTarget();
+        }   
+    }
+    public void Die()
+    {
+        navMeshAgent.speed = 0;
+        //velocity
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+        animator.SetTrigger("FallDown");
+        Destroy(this.gameObject, 5f);
+
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,24 +6,111 @@ using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 0;
+    private float speed = 0;
     public TextMeshProUGUI countText;
+    public TextMeshProUGUI messageText;
+    public TextMeshProUGUI usernameText;
+    public TextMeshProUGUI goodMessage;
+    public TextMeshProUGUI badMessage; 
+    public TextMeshProUGUI livesText;
     public GameObject winTextObject;
     private Rigidbody rb;
     private int count;
+    private int livesCount;
     private float movementX;
     private float movementY;
+    public List<string> messageList;
+    public List<string> usernameList;
+    public List<bool> isToxic;
+    private int currentMessage = 0;
+    private int currentUsername = 0;
+    public float messageInterval = 10f;
+    private float messageTimer;
+    public float feedbackMessageInterval = 5f;
+    private float feedbackMessageTimer;
+    private bool isFeedbackMessageShown = false;
+    private bool isToxiFollowing = true;
+    public float baseSpeed = 0;
+    public GameObject Enemy;
+    public AudioClip coin;
+    public AudioClip ribbit;
+    public AudioSource audioSource;
+    public Transform respawnPoint;
+    public Transform quizLocation;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        speed = baseSpeed;
         rb = GetComponent<Rigidbody>();
         count = 0;
+        livesCount = 3;
         SetCountText();
+        SetLivesText();
         winTextObject.SetActive(false);
+        ShowNextMessage();
+        goodMessage.gameObject.SetActive(false);
+        badMessage.gameObject.SetActive(false);
     }
 
-     void OnMove(InputValue movementValue)
+    void ShowNextMessage()
+    {
+        currentMessage = Random.Range(0, messageList.Count);
+        messageText.GetComponent<TextMeshProUGUI>().text = messageList[currentMessage];
+        currentUsername = Random.Range(0, usernameList.Count);
+        usernameText.GetComponent<TextMeshProUGUI>().text = usernameList[currentUsername];
+        if (isToxic[currentMessage] == true)
+        {
+            isToxiFollowing = false;
+        }
+    }
+
+    void OnReport()
+    {
+        isFeedbackMessageShown = true;
+        feedbackMessageTimer = 0f;
+        if (isToxic[currentMessage]== false)
+        {
+            speed = baseSpeed * 0.6f;
+            goodMessage.gameObject.SetActive(false);
+            badMessage.gameObject.SetActive(true);
+        }
+        else
+        {
+            speed = baseSpeed * 1.2f;
+            goodMessage.gameObject.SetActive(true);
+            badMessage.gameObject.SetActive(false);
+            Enemy.GetComponent<EnemyMovement>().stopChasingPlayer();
+            this.transform.position = quizLocation.position;
+        }
+    }
+
+
+
+    private void Update()
+    {
+        messageTimer = messageTimer + Time.deltaTime;
+        if (messageTimer > messageInterval)
+        {
+            ShowNextMessage();
+            messageTimer = 0f;
+           
+        }
+        if (isFeedbackMessageShown == true)
+        {
+            feedbackMessageTimer = feedbackMessageTimer + Time.deltaTime;
+            if (feedbackMessageTimer > feedbackMessageInterval)
+            {
+                goodMessage.gameObject.SetActive(false);
+                badMessage.gameObject.SetActive(false);
+                isFeedbackMessageShown = false;
+                feedbackMessageTimer = 0f;
+
+            }
+        }
+    }
+    void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
 
@@ -33,18 +120,32 @@ public class PlayerController : MonoBehaviour
 
     void SetCountText()
     {
-        countText.text = "Count: " + count.ToString();
-        if (count >= 18)
+        countText.text = "Count: " + count.ToString() + "/20";
+        if (count >= 20)
         {
             winTextObject.SetActive(true);
             Destroy(GameObject.FindGameObjectWithTag("Enemy"));
+            goodMessage.gameObject.SetActive(false);
+            badMessage.gameObject.SetActive(false);
+        }
+    }
+
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + livesCount.ToString() + "/3";
+        if (livesCount <= 0)
+        {
+            winTextObject.gameObject.SetActive(true);
+            winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
+            Destroy(gameObject);
+            goodMessage.gameObject.SetActive(false);
+            badMessage.gameObject.SetActive(false);
         }
     }
 
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-
         rb.AddForce(movement * speed);
     }
 
@@ -55,6 +156,8 @@ public class PlayerController : MonoBehaviour
             other.gameObject.SetActive(false);
             count = count + 1;
             SetCountText();
+            audioSource.clip = coin;
+            audioSource.Play();
         }
         
     }
@@ -63,11 +166,24 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            // Destroy the current object
-            Destroy(gameObject);
-            // Update the winText to display "You Lose!"
-            winTextObject.gameObject.SetActive(true);
-            winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
+            bool isFollowing = Enemy.GetComponent<EnemyMovement>().isFollowingPlayer;
+            if (isFollowing == true)
+            {
+                livesCount = livesCount - 1;
+                SetLivesText();
+                this.transform.position = respawnPoint.position;
+            }
+            else
+            {
+                winTextObject.SetActive(true);
+                audioSource.clip = ribbit;
+                audioSource.Play();
+                Enemy.GetComponent<EnemyMovement>().Die();
+                
+                goodMessage.gameObject.SetActive(false);
+                badMessage.gameObject.SetActive(false);
+            }
+            
         }
     }
 }
